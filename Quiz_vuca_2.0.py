f941199aa1fc67d6c803c9c041a0ac77609a4821
Snebur0101 <rubens.marques.profissional@gmail.com>
2025-01-27 12:42:58 -0300

    Update Quiz_vuca_2.0.py

--- Quiz_vuca_2.0.py
+++ Quiz_vuca_2.0.py
@@ -1,5 +1,6 @@
 import  streamlit as st
 import firebase_admin
+from firebase_admin import credentials, firestore
 import streamlit_authenticator as stauth
 import matplotlib.pyplot as plt
 import secrets
